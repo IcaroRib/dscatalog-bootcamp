51f2a8719b7043926ac63babc5526486c8677591
Glebson Santos <glebsonsantos@outlook.pt>
2021-09-19 13:47:29 -0300

    find by id category and Exception Handler

--- backend/src/main/java/com/bootcamp/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/bootcamp/dscatalog/services/CategoryService.java
@@ -1,6 +1,7 @@
 package com.bootcamp.dscatalog.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.bootcamp.dscatalog.dto.CategoryDTO;
 import com.bootcamp.dscatalog.entities.Category;
 import com.bootcamp.dscatalog.repositories.CategorygRepository;
+import com.bootcamp.dscatalog.services.exceptions.EntityNotFoundExeption;
 
 @Service
 public class CategoryService {
@@ -23,4 +25,10 @@ public class CategoryService {
 		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 	}
 	
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id){
+		Optional<Category> obj = repository.findById(id);
+		Category entity =  obj.orElseThrow(() -> new EntityNotFoundExeption("Entity Not found"));
+		return new CategoryDTO(entity);
+	}
 }
---
+++ backend/src/main/java/com/bootcamp/dscatalog/services/exceptions/EntityNotFoundExeption.java
@@ -0,0 +1,10 @@
+package com.bootcamp.dscatalog.services.exceptions;
+
+public class EntityNotFoundExeption extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public EntityNotFoundExeption(String msg) {
+		super(msg);
+	}
+
+}
