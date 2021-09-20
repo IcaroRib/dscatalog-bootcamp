b7dce8e1a70a2e42035e57b4878d081c59e5044c
Glebson Santos <glebsonsantos@outlook.pt>
2021-09-20 13:54:06 -0300

    update category

--- backend/src/main/java/com/bootcamp/dscatalog/resources/exception/ResourceExceptionHandler.java
+++ backend/src/main/java/com/bootcamp/dscatalog/resources/exception/ResourceExceptionHandler.java
@@ -9,13 +9,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import com.bootcamp.dscatalog.services.exceptions.EntityNotFoundExeption;
+import com.bootcamp.dscatalog.services.exceptions.ResourceNotFoundExeption;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
 
-	@ExceptionHandler(EntityNotFoundExeption.class)
-	public ResponseEntity<StandardError> entityNotFound(EntityNotFoundExeption e, HttpServletRequest request){
+	@ExceptionHandler(ResourceNotFoundExeption.class)
+	public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundExeption e, HttpServletRequest request){
 		StandardError err = new StandardError();
 		err.setTimestamp(Instant.now());
 		err.setStatus(HttpStatus.NOT_FOUND.value());
