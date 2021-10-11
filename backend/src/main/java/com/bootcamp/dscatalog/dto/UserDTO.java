f3e00c6306f0eaf1ff25e3608a4727c8b455fc35
Glebson Santos <glebsonsantos@outlook.pt>
2021-10-11 20:47:19 -0300

    Created entities User and Role and seed DB

--- backend/src/main/java/com/bootcamp/dscatalog/dto/UserDTO.java
+++ backend/src/main/java/com/bootcamp/dscatalog/dto/UserDTO.java
@@ -1,75 +1,70 @@
-package com.bootcamp.dscatalog.entities;
+package com.bootcamp.dscatalog.dto;
 
-import java.util.Set;
 import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.persistence.Table;
+import com.bootcamp.dscatalog.entities.Role;
+import com.bootcamp.dscatalog.entities.User;
 
 
-@Entity
-@Table(name = "tb_user")
-public class User implements Serializable{
-	
+public class UserDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
 	private String firstName;
 	private String lastName;
 	private String email;
 	private String password;
 	
-	@ManyToMany
-	@JoinTable(name = "tb_user_role", joinColumns = @JoinColumn(name = "user_id"),
-			   inverseJoinColumns = @JoinColumn(name = "role_id"))
-	private Set<Role> roles = new HashSet<>();
+	private Set<RoleDTO> roles = new HashSet<>();
 	
-	public User() {
+	public UserDTO() {
 	}
 	
-	public User(Long id, String firstName, String lastName, String email, String password) {
+	public UserDTO(Long id, String firstName, String lastName, String email, String password, Set<Role> roles) {
 		this.id = id;
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.email = email;
 		this.password = password;
+		//roles =
 	}
-
+	
+	public UserDTO(User entity) {
+		this.id = entity.getId();
+		this.firstName = entity.getFirstName();
+		this.lastName = entity.getLastName();
+		this.email = entity.getEmail();
+		this.password = entity.getPassword();
+		//roles = entity.getRoles().stream().map(x -> new RoleDTO(x))entity;
+	}
+	
 	public Long getId() {
 		return id;
 	}
-
+	
 	public void setId(Long id) {
 		this.id = id;
 	}
-
+	
 	public String getFirstName() {
 		return firstName;
 	}
-
+	
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
-
+	
 	public String getLastName() {
 		return lastName;
 	}
-
+	
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
-
+	
 	public String getEmail() {
 		return email;
 	}
@@ -81,20 +76,19 @@ public class User implements Serializable{
 	public String getPassword() {
 		return password;
 	}
-
+	
 	public void setPassword(String password) {
 		this.password = password;
 	}
 	
-	public Set<Role> getRoles() {
+	public Set<RoleDTO> getRoles() {
 		return roles;
 	}
-
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
-
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -103,7 +97,7 @@ public class User implements Serializable{
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		User other = (User) obj;
+		UserDTO other = (UserDTO) obj;
 		return Objects.equals(id, other.id);
 	}
 }
